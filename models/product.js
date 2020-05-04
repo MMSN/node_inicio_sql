bdc9507d8f2532a71caedeb81eee2da31a48fbd4
Mateus Moreira Silveira do Nascimento <mateus@Mateuss-MacBook-Pro.local>
2020-05-04 19:11:40 -0300

    Parte do codigo refatorada (c comentario), seguindo modelo MVC

--- models/product.js
+++ models/product.js
@@ -5,37 +5,78 @@ const path = require('path');
 // remover este vetor para guardar as infos num arquivo
 //const products = [];
 
+//Global
+const p = path.join(
+        path.dirname(process.mainModule.filename), 
+        'data', 
+        'products.json'
+    );
+
+const getProductsFromFile = (cb) => {
+    //const p = path.join(
+    //    path.dirname(process.mainModule.filename), 
+    //    'data', 
+    //    'products.json'
+    //);
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            //ao usar cb nao posso mais retornar o array
+            //return [];
+            cb([]);
+        } else {
+            cb(JSON.parse(fileContent));
+        }
+        //return JSON.parse(fileContent);
+        //cb(JSON.parse(fileContent));
+    });
+}
+
 module.exports = class Product {
     constructor(t) {
         this.title = t;
     }
     
     save() {
-        //essa primeira linha eh p o vetor que nao existe mais
-        //products.push(this);
-        
-        //dentro do join esta a linha que existia no util, para 
-        //encontrar o diretorio onde salvarei e carregarei o file
-        //'data' eh o diretorio, 'products' o arquivo
-        const p = path.join(
-            path.dirname(process.mainModule.filename), 
-            'data', 
-            'products.json'
-        );
-        fs.readFile(p, (err, fileContent) => {
-            let products = [];
-            if (!err) {
-                products = JSON.parse(fileContent);
-            }
+        //cb nao eh necessario nessa, visto que a
+        //logica aqui eh diferente
+        getProductsFromFile(products => {
             products.push(this);
-            fs.writeFile(p, JSON.stringify(products), (err) => {
+            fs.writeFile(p, JSON.stringify(products), err => {
                 console.log(err);
             });
         });
+        
+        //essa primeira linha eh p o vetor que nao existe mais
+        //products.push(this);
+
+        //acabou sendo comentanda devido a inclusao da func auxiliar
+        
+        //dentro do join esta a linha que existia no util, para 
+        //encontrar o diretorio onde salvarei e carregarei o file
+        //'data' eh o diretorio, 'products' o arquivo        
+        //const p = path.join(
+        //    path.dirname(process.mainModule.filename), 
+        //    'data', 
+        //    'products.json'
+        //);
+        //fs.readFile(p, (err, fileContent) => {
+        //    let products = [];
+        //    if (!err) {
+        //        products = JSON.parse(fileContent);
+        //    }
+        //    products.push(this);
+        //    fs.writeFile(p, JSON.stringify(products), (err) => {
+        //        console.log(err);
+        //    });
+        //});
     }
     
     static fetchAll(cb) {
-        const p = path.join(
+        getProductsFromFile(cb);
+        
+        //com a adicao da getProductsFromFile nao ha mais necessidade
+        //deste trecho de codigo, pois foi p aquela funcao
+        /*const p = path.join(
             path.dirname(process.mainModule.filename), 
             'data', 
             'products.json'
@@ -49,7 +90,7 @@ module.exports = class Product {
             }
             //return JSON.parse(fileContent);
             cb(JSON.parse(fileContent));
-        });
+        });*/
         
         //nao mais necessario visto que nao ha mais o array
         //return products;
